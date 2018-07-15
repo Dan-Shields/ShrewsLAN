44a272c13bf688728af5bc35af3ae727d4983f9e
Dan Shields <d.shieldsuk@gmail.com>
2018-07-16 00:46:45 +0100

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
