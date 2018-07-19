802ac3085eb321b6fc6df17938c37131d3d3e9fe
Dan Shields <d.shieldsuk@gmail.com>
2018-07-19 14:40:10 +0100

    Add header containing logo and countdown

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <h1 class="title"><span style="font-weight: 200">S<span style="font-size: 80px">HREWS</span></span>LAN</h1>
+    <Countdown date="September 1, 2018 10:00"></Countdown>
+  </div>
+</template>
+
+<script>
+import Countdown from '@/components/Countdown'
+
+export default {
+  name: 'Header',
+
+  data () {
+    return {
+    }
+  },
+
+  components: {
+    Countdown
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 100px;
+  position: relative;
+  margin-top: 0;
+  padding-top: 50px;
+  width: auto;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="home">
-
+    <Header></Header>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
 export default {
   name: 'Home',
+
   data () {
     return {
     }
+  },
+
+  components: {
+    Header,
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.home {
+  width: 1200px;
+  margin: 0px auto;
+}
 </style>
