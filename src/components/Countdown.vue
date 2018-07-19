03422d480e8a1e0ecee4fc0538875f6ed5fb0535
Dan Shields <d.shieldsuk@gmail.com>
2018-07-19 14:39:26 +0100

    Add countdown component

---
+++ src/components/Countdown.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="countdown">
+    <div class="block">
+      <p class="digit">{{ days | two_digits }}</p>
+      <p class="text">Days</p>
+    </div>
+    <div class="block">
+      <p class="digit">{{ hours | two_digits }}</p>
+      <p class="text">Hours</p>
+    </div>
+    <div class="block">
+      <p class="digit">{{ minutes | two_digits }}</p>
+      <p class="text">Minutes</p>
+    </div>
+    <div class="block">
+      <p class="digit">{{ seconds | two_digits }}</p>
+      <p class="text">Seconds</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    window.setInterval(() => {
+      this.now = Math.trunc((new Date()).getTime() / 1000)
+    }, 1000)
+  },
+
+  props: {
+    date: String
+  },
+
+  data () {
+    return {
+      now: Math.trunc((new Date()).getTime() / 1000)
+    }
+  },
+
+  computed: {
+    seconds () {
+      return (this.timestamp - this.now) % 60
+    },
+
+    minutes () {
+      return Math.trunc((this.timestamp - this.now) / 60) % 60
+    },
+
+    hours () {
+      return Math.trunc((this.timestamp - this.now) / 60 / 60) % 24
+    },
+
+    days () {
+      return Math.trunc((this.timestamp - this.now) / 60 / 60 / 24)
+    },
+
+    timestamp () {
+      return Math.trunc(Date.parse(this.date) / 1000)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.countdown {
+  display: flex;
+  margin: auto;
+  justify-content: center;
+}
+
+.block {
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+}
+
+.text {
+  color: #00cecb;
+  font-size: 25px;
+  font-weight: 40;
+  margin-top:10px;
+  margin-bottom: 10px;
+  text-align: center;
+}
+
+.digit {
+  color: #ecf0f1;
+  font-size: 75px;
+  font-weight: 100;
+  margin: 10px;
+  text-align: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.filter('two_digits', function (value) {
+  if (value.toString().length <= 1) {
+    return '0' + value.toString()
+  }
+
+  return value.toString()
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
