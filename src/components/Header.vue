5a042c963886bab55ca1da93724aecd8db0fc465
Dan Shields <d.shieldsuk@gmail.com>
2018-08-06 00:18:07 +0300

    Improved header layout with new subtitle and logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="header">
-    <h1 class="title"><span style="font-weight: 200">S<span style="font-size: 80px">HREWS</span></span>LAN</h1>
+    <div class="banner">
+      <div class="logo">
+        <img src="@/assets/logo.png"/>
+      </div>
+
+      <h1 class="title"><span style="font-weight: 200">S<span style="font-size: 80px">HREWS</span></span>LAN</h1>
+    </div>
+
+    <p class="subtitle">September 1<sup>st</sup>-2<sup>nd</sup> 2018 <span style="font-weight: 800; margin: auto 10px;">&#8226; </span>Mount Pleasant Primary School, Shrewsbury</p>
     <Countdown :date="date"></Countdown>
   </div>
 </template>
@@ -27,11 +35,46 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.title {
-  font-size: 100px;
+.header {
+  margin-bottom: 50px;
+}
+
+.banner {
+  display: flex;
+  justify-content: center;
   position: relative;
-  margin-top: 0;
-  padding-top: 50px;
-  width: auto;
+  align-items: baseline;
+  user-select: none;
+
+  .logo {
+    flex-basis: 120px;
+
+    img {
+      height: 100px;
+      transform: translateY(10%);
+    }
+  }
+
+  .title {
+    font-size: 100px;
+    margin: 0;
+    padding-top: 50px;
+    width: auto;
+  }
+}
+
+.subtitle {
+  font-weight: 200;
+  font-size: 20px;
+  clear: both;
+}
+
+.countdown {
+  clear: both;
+  margin-bottom: 25px !important;
+}
+
+.signup {
+  margin: auto;
 }
 </style>
