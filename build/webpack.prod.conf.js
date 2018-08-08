324d803bf2ce119d59a0f930c740334db68c54bf
Dan Shields <d.shieldsuk@gmail.com>
2018-08-08 14:43:07 +0300

    Include .htaccess when building for prod

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -118,6 +118,10 @@ const webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../static/.htaccess'),
+        to: config.build.assetsRoot        
       }
     ])
   ]
