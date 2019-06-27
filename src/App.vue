214f6b081bd3de9bf35ac4a329151356653319ce
Dan Shields <d.shieldsuk@gmail.com>
2019-06-27 16:06:15 +0100

    feat: add styled scrollbar (webkit only)

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,35 @@
 </template>
 
 <style lang="scss">
+@import './assets/scss/global.scss';
+
+$background: #02111b;
+$scrollbar-width: 20px;
+
 html {
-  background-color: #02111b;
+  background-color: $background;
+
+  &::-webkit-scrollbar-track
+  {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    background-color: lighten($background, 5%);
+  }
+
+  &::-webkit-scrollbar
+  {
+    width: $scrollbar-width;
+    background-color: lighten($background, 5%);
+  }
+
+  &::-webkit-scrollbar-thumb
+  {
+    border-radius: $scrollbar-width / 2;
+    background-image: -webkit-gradient(linear,
+                                       left bottom,
+                                       left top,
+                                       color-stop(0.15, $primary),
+                                       color-stop(0.85, $secondary));
+  }
 }
 
 #app {
