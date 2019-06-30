d6b35b803f3cec15fedcff1abde2d2e410669d2f
Dan Shields <d.shieldsuk@gmail.com>
2019-06-30 18:00:09 +0100

    fix: move checkmark svg to separate file

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -46,29 +46,7 @@
 
                 <div class="modal-body">
                   <div class="checkmark" :class="{draw: drawCheckmark}">
-                    <svg
-                      version="1.1"
-                      id="tick"
-                      xmlns="http://www.w3.org/2000/svg"
-                      xmlns:xlink="http://www.w3.org/1999/xlink"
-                      x="0px"
-                      y="0px"
-                      viewBox="0 0 37 37"
-                      style="enable-background:new 0 0 37 37;"
-                      xml:space="preserve">
-
-                      <path
-                        class="circ path"
-                        style="fill:none;stroke:#39F06A;stroke-width:2;stroke-linejoin:round;stroke-miterlimit:10;"
-                        d="M30.5,6.5L30.5,6.5c6.6,6.6,6.6,17.4,0,24l0,0c-6.6,6.6-17.4,6.6-24,0l0,0c-6.6-6.6-6.6-17.4,0-24l0,0C13.1-0.2,23.9-0.2,30.5,6.5z"
-                      />
-
-                      <polyline
-                        class="tick path"
-                        style="fill:none;stroke:#39F06A;stroke-width:2;stroke-linejoin:round;stroke-miterlimit:10;"
-                        points="11.6,20 15.9,24.2 26.4,13.8 "/>
-
-                    </svg>
+                    <SuccessIcon></SuccessIcon>
                   </div>
                   <p>You will shortly receive an email confirming your registration and providing details on how to attend the event. See you there!</p>
                 </div>
@@ -89,7 +67,7 @@
 
 <script>
 import SignupForm from '@/components/SignupForm';
-import GridLoader from 'vue-spinner/src/GridLoader.vue';
+import SuccessIcon from '@/assets/img/checkmark.svg?inline';
 import CustomButton from '@/components/CustomButton.vue';
 
 export default {
